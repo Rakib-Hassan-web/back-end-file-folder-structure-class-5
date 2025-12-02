c049578576f74fd315332c8049c76680aecaab9d
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-02 19:13:26 +0600

    reg auth complete

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -21,10 +21,7 @@ const registration = async (req, res) => {
             password
         });
 
-        await newUser.save(); 
-
-        res.status(201).send("registration successful");
-
+   
     } catch (error) {
         console.log(error);
         res.status(500).send("Something went wrong");
